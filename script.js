b52d99adb555d11d60d975dacc655ec2c0ff3664
MohamedArtamity <mohamedartamity@gmail.com>
2026-07-13 14:59:08 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+// Smooth scrolling and future interactions
 
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+    link.addEventListener('click', function(e){
+
+        const target = document.querySelector(this.getAttribute('href'));
+
+        if(target){
+
+            e.preventDefault();
+
+            target.scrollIntoView({
+                behavior:"smooth"
+            });
+
+        }
+
+    });
+
+});
